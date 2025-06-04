8d7499f1acc518dde5d98ca16aa7cf1563fe2c50
Ricardo Nunton Garcia <ricardocanuntongarcia@gmail.com>
2025-06-04 01:06:42 -0500

    cambios visuales

--- Secuenciales/07.py
+++ Secuenciales/07.py
@@ -1,12 +1,11 @@
 import os
-
 os.system("cls")
 
-base = int(input("Base = "))
-altura = int(input("Altura = "))
+base = int ( input ( "Base = " ))
+altura = int ( input ( "Altura = " ))
 
 area = base * altura
 perimetro = 2 * ( base + altura)
 
-print(f"Area = {area:.2f}")
-print(f"Perimetro = {perimetro:.2f}")
+print ( f"Perimetro = {perimetro:.2f} " )
+print ( f"Area = {area:.2f} " )
