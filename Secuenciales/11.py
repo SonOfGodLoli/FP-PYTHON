63651c7300e89f3b8b023f2d783da1ec5781b80b
SonOfGodLoli <ricardocanuntongarcia@gmail.com>
2025-06-06 06:41:47 -0500

    Nuevos ejercicicios

---
+++ Secuenciales/11.py
@@ -0,0 +1,5 @@
+#Dado  dos  números  enteros  de  3  cifras,  desarrolle  el  programa  que  muestre  la  cifra  primera  y tercera cifras intercambiadas entre ambos números. Ejemplo 123 y 456 → 624 y 351
+
+import os
+os.system("cls")
+
