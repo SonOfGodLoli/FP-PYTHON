2e930533cfd825ae8972044a427ea6b6c13b1611
SonOfGodLoli <ricardocanuntongarcia@gmail.com>
2025-06-16 17:17:14 -0500

    ##

--- PYTHON/Secuenciales/20.py
+++ PYTHON/Secuenciales/20.py
@@ -6,7 +6,36 @@ os.system("cls")
 Dinero = int(input ("Ingrese una cantidad de dinero : "))
 
 D200 = Dinero // 200
-D100 = Dinero - ( D200 * 200 )
+Dinero = Dinero % 200
+
+D100 = Dinero // 100
+Dinero = Dinero % 100
+
+D50 = Dinero // 50
+Dinero = Dinero % 50
+
+D20 = Dinero // 20
+Dinero = Dinero % 20
+
+D10 = Dinero // 10
+Dinero = Dinero % 10
+
+D5 = Dinero // 5
+Dinero = Dinero % 5
+
+D2 = Dinero // 2
+Dinero = Dinero % 2
+
+D1 = Dinero // 1
+Dinero = Dinero % 1
+
+
+
 
 print(D200)
 print(D100)
+print(D50)
+print(D20)
+print(D5)
+print(D2)
+print(D1)
