8463a9875dfcbf7745641d51b82601b182dfcd4f
SonOfGodLoli <ricardocanuntongarcia@gmail.comgit config --global user.email ricardocanuntongarcia@gmail.com>
2025-06-24 18:34:45 -0500

    A

---
+++ PYTHON/Condicionales/14C.py
@@ -0,0 +1,4 @@
+#En un supermercado hay una promoción según la cual el cliente raspa una tarjeta que contiene un 
+#número oculto. Si el número de la tarjeta es par no menor de 100, el cliente obtiene un descuento 
+#del 15 %, caso contrario será del 5 % sobre el importe de la compra.  Desarrolle el programa que 
+#muestre el número de la tarjeta, el monto de la compra y el descuento.
