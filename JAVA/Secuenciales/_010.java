8d7338009fac617a1563ea5263a6c78a76241dee
SonOfGodLoli <ricardocanuntongarcia@gmail.comgit config --global user.email ricardocanuntongarcia@gmail.com>
2025-07-01 21:31:24 -0500

    555

---
+++ JAVA/Secuenciales/_010.java
@@ -0,0 +1,23 @@
+// Dado un número natural de cuatro cifras, desarrolle el programa que permite obtener el número al revés. Ejemplo 1234  → 4321
+
+package JAVA.Secuenciales;
+
+import java.util.Scanner;
+
+
+public class _010 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa el numero : ");
+        int numero = scanner.nextInt();
+
+        int nuevoN = (numero%10)*1000 + ((numero%100)/10)*100 + ((numero/100)%10)*10 + numero/1000;
+
+        System.out.println(String.format("numero nuevo : ", nuevoN));
+
+        scanner.close();
+    }
+}
--- JAVA/Secuenciales/_05.java
+++ JAVA/Secuenciales/_05.java
@@ -1,5 +1,28 @@
-package Secuenciales;
+// Desarrolle  el  programa  que,  dada  la  capacidad  de  un  disco  duro  en  gigabytes,  lo  convierta  a megabytes, kilobytes y bytes. 1 KB = 1024 bytes, 1 MB = 1024 KB, 1 GB = 1024 MB.
+
+package JAVA.Secuenciales;
+
+import java.util.Scanner;
 
 public class _05 {
     
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Gigabytes : ");
+        int Gigabytes = scanner.nextInt();
+
+        double megabytes = Gigabytes * 1024 ;
+        double kilobytes = megabytes * 1024 ;
+        double bytes = kilobytes * 1024 ;
+
+        System.out.println("");
+        System.out.println("Megabytes : " + megabytes);
+        System.out.println("Kilobytes : " + kilobytes);
+        System.out.println(String.format("Kilobytes : ", kilobytes));
+        System.out.println("Bytes : " + bytes);
+
+        scanner.close();
+    }
 }
