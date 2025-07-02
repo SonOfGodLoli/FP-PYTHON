13fe4b9fb8c062b220c42ef614d75ea5641faa47
SonOfGodLoli <ricardocanuntongarcia@gmail.com>
2025-07-01 23:35:33 -0500

    &

--- JAVA/Secuenciales/_05.java
+++ JAVA/Secuenciales/_05.java
@@ -1,5 +1,27 @@
-package Secuenciales;
+// Desarrolle  el  programa  que,  dada  la  capacidad  de  un  disco  duro  en  gigabytes,  lo  convierta  a megabytes, kilobytes y bytes. 1 KB = 1024 bytes, 1 MB = 1024 KB, 1 GB = 1024 MB.
 
+package JAVA.Secuenciales;
+
+import java.util.Scanner;
 public class _05 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad en gigabytes : ");
+        int Giga = scanner.nextInt();
+
+        int megab = Giga * 1024; 
+        int Kilob = megab * 1024; 
+        int bytes = Kilob * 1024; 
+
+        System.out.println("Cantidad Transformada a megas : " + megab +".");
+        System.out.println("Cantidad Transformada a kilobites : " + Kilob +".");
+        System.out.println("Cantidad Transformada a bytes : " + bytes +".");
+
+        scanner.close();
+    }
     
+
 }
--- JAVA/Secuenciales/_06.java
+++ JAVA/Secuenciales/_06.java
@@ -1,9 +1,33 @@
-package Secuenciales;
+// Desarrolle  el  programa  que  calcule  el  área  total  y  el  volumen  de  un  cilindro.  Considere  las siguientes formulas: Área = 2πr(r+h) y Volumen = πr2h. Siendo r el radio y h la altura
+package JAVA.Secuenciales;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class _06 {
 
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el radio del cilindro : ");
+        int radio = scanner.nextInt();
+
+        System.out.print("Ingrese la altura del cilindro : ");
+        int altura = scanner.nextInt();
+
+        double area = (2*(Math.PI) * radio)* (radio + altura);
+        double volumen = Math.PI * (Math.pow(radio, 2))* (altura);
+
+
+        DecimalFormat  df = new DecimalFormat("#0.00"); 
+        System.out.println("");
+        System.out.println("EL area del cilindro es : "+df.format(area)+" cm.");
+        System.out.println("El volumen del cilindro es : "+ df.format(volumen) + " cm.");
+
+        scanner.close();
+
+
     }
     
 }
