0574a9a23753d536f43324a6eb8fcf9cd25ca821
SonOfGodLoli <ricardocanuntongarcia@gmail.comgit config --global user.email ricardocanuntongarcia@gmail.com>
2025-07-09 22:18:44 -0500

    $$$$

---
+++ PYTHON/Condicionales/40C.py
@@ -0,0 +1,9 @@
+# En  un  instituto  los  cursos  de  matemática,  física  y  química  se  evalúan  en  base  a  tres  prácticas 
+# calificadas, examen parcial y final. Cada práctica tiene un peso dado. Desarrolle el programa que 
+# determine el promedio final del curso y su condición de aprobado / desaprobado dado que la nota 
+# mínima es de 13. 
+# Curso PC1 PC2 PC3 EP EF 
+#  Matemática 10 % 20 % 10 % 30 % 30 % 
+# Física 20 % 20 % 20 % 20 % 20 % 
+# Química 10 % 30 % 10 % 25 % 25 % 
+
